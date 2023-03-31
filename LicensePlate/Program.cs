385b7e6169436855e82f5c3b075d745f5f917c17
nanthakumaran-s <nanthankumaransenthil@gmail.com>
2023-03-31 16:12:29 +0530

    Initial Commit

---
+++ LicensePlate/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string plateNumber = LicensePlate("5F3Z-2e-9-w", 4);
+        Console.WriteLine(plateNumber);
+    }
+
+    public static string LicensePlate(string s, int n)
+    {
+        s = s.ToUpper().Replace("-", "");
+        int len = s.Length;
+        List<string> result = new();
+        for (int i = len - 1; i >= 0; i--)
+        {
+            result.Add(s[i].ToString());
+            int lenOfRes = result.Count - result.Count(x => x.Contains("-"));
+            if (lenOfRes % n == 0)
+            {
+                result.Add("-");
+            }
+        }
+        result.Reverse();
+        if (result[0] == "-")
+        {
+            result.RemoveAt(0);
+        }
+        return string.Join("", result.ToArray());
+    }
+}
---
+++ LicensePlateTests/ProgramTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void Test1()
+        {
+            Assert.IsTrue(Program.LicensePlate("5F3Z-2e-9-w", 4) == "5F3Z-2E9W");
+        }
+
+        [TestMethod()]
+        public void Test2()
+        {
+            Assert.IsTrue(Program.LicensePlate("2-5g-3-J", 2) == "2-5G-3J");
+        }
+
+        [TestMethod()]
+        public void Test3()
+        {
+            Assert.IsTrue(Program.LicensePlate("2-4A0r7-4k", 3) == "24-A0R-74K");
+        }
+
+        [TestMethod()]
+        public void Test4()
+        {
+            Assert.IsTrue(Program.LicensePlate("nlj-206-fv", 3) == "NL-J20-6FV");
+        }
+    }
+}
